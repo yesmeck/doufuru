0b21e128f35a82e132ccdec4811483c9dd897308
Meck <yesmeck@gmail.com>
2012-09-21 00:47:51 +0800

    增加根据 isbn 获取图书

--- lib/doufuru/client/book.rb
+++ lib/doufuru/client/book.rb
@@ -6,6 +6,10 @@ module Doufuru
       def book(id)
         get("book/#{id}")
       end
+
+      def book_by_isbn(isbn)
+        get("book/isbn/#{isbn}")
+      end
     end
   end
 end
--- spec/client/book_spec.rb
+++ spec/client/book_spec.rb
@@ -10,8 +10,20 @@ describe Doufuru::Client do
   describe ".book" do
     context "with a id passed" do
       it "should return the book" do
-        stub_get("/book/1084336").to_return(:body => fixture("book.json"))
-        book = @client.book(1084336)
+        id = 1084336
+        stub_get("/book/#{id}").to_return(:body => fixture("book.json"))
+        book = @client.book(id)
+        book.title.should == "小王子"
+      end
+    end
+  end
+
+  describe ".book_by_isbn" do
+    context "with a isbn id passed" do
+      it "should return the book" do
+        isbn = 9787020042494
+        stub_get("/book/isbn/#{isbn}").to_return(:body => fixture("book.json"))
+        book = @client.book_by_isbn(isbn)
         book.title.should == "小王子"
       end
     end
