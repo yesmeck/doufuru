9f58895f23826fa916ecdc4b8613ce340d6b0237
Meck <yesmeck@gmail.com>
2012-09-22 01:03:41 +0800

    a => the

--- spec/client/book_spec.rb
+++ spec/client/book_spec.rb
@@ -96,7 +96,7 @@ describe Doufuru::Client do
 
   describe ".update_book_review" do
     context "with a review id passed" do
-      it "should update a book review" do
+      it "should update the book review" do
         review_content = "因为，绳命，是剁么的回晃；绳命，是入刺的井猜。壤窝们，巩痛嘱咐碰优。田下冯广宰饿妹，饿妹冯广宰呲处。壤窝们，嘱咐这缩优类缩优。开心的一小，火大的一小，壤绳命，梗楤容，壤绳命，梗秤巩，壤绳命，梗回晃。"
 
         stub_put("/book/review/#{@review_id}").with(
