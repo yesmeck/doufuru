a0b2c347030e86f3abf2ed8003d5e77fbdf9a8da
Meck <yesmeck@gmail.com>
2012-09-22 14:15:24 +0800

    [Pending]增加用户对电影的所有标签

--- spec/client/movie_spec.rb
+++ spec/client/movie_spec.rb
@@ -137,4 +137,15 @@ describe Doufuru::Client do
       end
     end
   end
+
+  describe ".movie_user_tags" do
+    context "with a movie id passed" do
+      it "should return oauthed user's tags of the movie" do
+        pending("豆瓣的返回好像有问题，获取不到标签")
+        stub_get("/movie/user_tags/#{@movie_id}").with(:headers => {
+          "Authorization" => "Bearer #{@access_token}"
+        }).to_return(:body => fixture("movie_user_tags.json"))
+      end
+    end
+  end
 end
