18f7f24c1d88aeaa42c03670ade31fe8ba5decad
Meck <yesmeck@gmail.com>
2012-09-22 12:47:35 +0800

    增加根据 imdb 获取电影信息

--- lib/doufuru/client/movie.rb
+++ lib/doufuru/client/movie.rb
@@ -6,6 +6,10 @@ module Doufuru
       def movie(id, params = {})
         get("movie/#{id}", params)
       end
+
+      def movie_by_imdb(imdb, params = {})
+        get("movie/imdb/#{imdb}", params)
+      end
     end
   end
 end
--- spec/client/movie_spec.rb
+++ spec/client/movie_spec.rb
@@ -7,6 +7,7 @@ describe Doufuru::Client do
     @access_token = "myfaketoken"
     @movie_id = 1307931
     @movie_title = "Peter Pan"
+    @imdb = "tt0316396"
     @client = Doufuru::Client.new(:access_token => @access_token)
   end
 
@@ -16,6 +17,17 @@ describe Doufuru::Client do
         stub_get("/movie/#{@movie_id}").to_return(:body => fixture("movie.json"))
 
         movie = @client.movie(@movie_id)
+        movie.title.should == @movie_title
+      end
+    end
+  end
+
+  describe ".movie_by_imdb" do
+    context "with a imdb passed" do
+      it "should return the movie" do
+        stub_get("/movie/imdb/#{@imdb}").to_return(:body => fixture("movie.json"))
+
+        movie = @client.movie_by_imdb(@imdb)
         movie.title.should == @movie_title
       end
     end
