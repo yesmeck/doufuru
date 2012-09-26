0627cbbadeca7b397d7373860325665c80d0ca43
Meck <yesmeck@gmail.com>
2012-09-26 21:40:10 +0800

    增加更新讨论

--- lib/doufuru/client/discussion.rb
+++ lib/doufuru/client/discussion.rb
@@ -6,6 +6,10 @@ module Doufuru
       def discussion(id, params = {})
         get("/discussion/#{id}", params)
       end
+
+      def update_discussion(id, params = {})
+        put("/discussion/#{id}", params)
+      end
     end
   end
 end
--- spec/client/discussion_spec.rb
+++ spec/client/discussion_spec.rb
@@ -6,6 +6,7 @@ describe Doufuru::Client do
   before do
     @discussion_id = 11676229
     @discussion_title =  "温蒂她爹就是虎克船长- =？"
+    @discussion_content = "测试内容"
   end
 
   describe ".discussion" do
@@ -15,6 +16,23 @@ describe Doufuru::Client do
 
         discussion = @client.discussion(@discussion_id)
         discussion.title.should eq @discussion_title
+      end
+    end
+  end
+
+  describe ".update_discussion" do
+    context "with a id passed" do
+      it "should update the discussion" do
+        stub_put("/discussion/#{@discussion_id}").
+          with(
+            :content => { :title => @discussion_title, :content => @discussion_content },
+            :headers => { "Authorization" => "Bearer #{@access_token}" }
+          ).
+          to_return(:body => fixture("discussion_update.json"))
+
+        discussion = @client.update_discussion(@discussion_id)
+        discussion.title.should eq @discussion_title
+        discussion.content.should eq @discussion_content
       end
     end
   end
