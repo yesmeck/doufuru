04b4b1478d2503d0391ed2e26efa4008ce850596
Meck <yesmeck@gmail.com>
2012-10-17 21:08:06 +0800

    修正带图片的广播不能正确发送的问题

--- lib/doufuru.rb
+++ lib/doufuru.rb
@@ -1,5 +1,27 @@
 # encoding: utf-8
 
+# MultiParts 计算的 Content-Lenght 跟豆瓣服务器的计算结果不一至，导致请求失败，
+# 所以没办法只能在这里打个 monkey patch。
+require 'parts'
+
+class Object
+  def require(file)
+    if file != "parts"
+      super
+    end
+  end
+end
+
+module Parts
+  class FilePart
+    alias origin_initialize initialize
+    def initialize(boundary, name, io)
+      origin_initialize(boundary, name, io)
+      @length -= @foot.length
+    end
+  end
+end
+
 require "doufuru/version"
 require "doufuru/configuration"
 require "doufuru/client"
