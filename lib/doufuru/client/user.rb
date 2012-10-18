ada80877d14ac0e121b8ed82f3ad9863fa0ec74b
Meck <yesmeck@gmail.com>
2012-10-18 21:38:30 +0800

    增加获取用户关注列表

--- lib/doufuru/client/user.rb
+++ lib/doufuru/client/user.rb
@@ -4,11 +4,15 @@ module Doufuru
   class Client
     module User
       def user(user = '~me', params = {})
-        get("/user/#{user}")
+        get("/user/#{user}", params)
       end
 
       def search_users(params = {})
         get("/user", params)['users']
+      end
+
+      def user_following(uid, params = {})
+        get("/shuo/users/#{uid}/following", params)
       end
     end
   end
--- spec/client/user_spec.rb
+++ spec/client/user_spec.rb
@@ -5,12 +5,16 @@ require "spec_helper"
 describe Doufuru::Client::User do
   include_context "initialize client"
 
+  before do
+    @uid = "coolzi"
+  end
+
   describe ".user" do
     context "with a uid passed" do
       it "should return the user" do
-        stub_get("/user/coolzi").to_return(:body => fixture("user.json"))
-        user = @client.user("coolzi")
-        user.uid.should == "coolzi"
+        stub_get("/user/#{@uid}").to_return(:body => fixture("user.json"))
+        user = @client.user(@uid)
+        user.uid.should == @uid
       end
     end
 
@@ -21,7 +25,7 @@ describe Doufuru::Client::User do
           :headers => { "Authorization" => "Bearer #{@access_token}" }
         )
         user = @client.user
-        user.uid.should == "coolzi"
+        user.uid.should == @uid
       end
     end
   end
@@ -30,10 +34,22 @@ describe Doufuru::Client::User do
     context "with a keyword passed" do
       it "should return matching users" do
         stub_get("/user").
-          with(:query => { :q => "coolzi", :start => 0, :count => 10 }).
+          with(:query => { :q => @uid, :start => 0, :count => 10 }).
           to_return(:body => fixture("users.json"))
-        user = @client.search_users({ :q => "coolzi", :start => 0, :count => 10 })
-        user.first.uid.should == "coolzi"
+        user = @client.search_users({ :q => @uid, :start => 0, :count => 10 })
+        user.first.uid.should == @uid
+      end
+    end
+  end
+
+  describe ".user_following" do
+    context "with a user id passed" do
+      it "should return the user's following list." do
+        stub_get("/shuo/users/#{@uid}/following").
+          to_return(:body => fixture("user_following.json"))
+
+        users = @client.user_following(@uid)
+        users.first.uid.should eq "zhutousecretary"
       end
     end
   end
