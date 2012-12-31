71d00560655469302b2448f24cef05449a98b6fe
Meck <yesmeck@gmail.com>
2012-12-31 18:24:11 +0800

    Add Books#search

--- lib/doufuru/books.rb
+++ lib/doufuru/books.rb
@@ -6,6 +6,7 @@ module Doufuru
     def get(id, params = {})
       set :id => id
       assert_presence_of id
+      normalize! params
 
       get_request("/book/#{id}", params)
     end
@@ -14,10 +15,18 @@ module Doufuru
     def get_by_isbn(isbn, params = {})
       set :isbn => isbn
       assert_presence_of isbn
+      normalize! params
 
       get_request("/book/isbn/#{isbn}", params)
     end
     alias :find_by_isbn :get_by_isbn
 
+    def search(params = {})
+      normalize! params
+      assert_presence_of params["q"] if params["tag"].nil?
+      assert_presence_of params["tag"] if params["q"].nil?
+
+      get_request("/book/search", params).books
+    end
   end
 end
---
+++ spec/doufuru/books/search_spec.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+require "spec_helper"
+
+describe Doufuru::Books, "#search" do
+
+  let(:keyword) { "小王子" }
+  let(:tag) { "小王子" }
+  let(:request_path) { "/book/search" }
+
+  before do
+    stub_get(request_path).with(:query => query).to_return(:body => body)
+  end
+
+  context "with keyword passed" do
+    let(:query) { {:q => keyword} }
+    let(:body) { fixture("books/books.json") }
+
+    it "should get the books" do
+      books = subject.search :q => keyword
+      books.first.title.should == "小王子"
+    end
+  end
+
+  context "with tag passed" do
+    let(:query) { {:tag => tag} }
+    let(:body) { fixture("books/books.json") }
+
+    it "should get the books" do
+      books = subject.search :tag => tag
+      books.first.title.should == "小王子"
+    end
+  end
+
+end
